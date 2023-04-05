216aace800fded54e4a84f31dd718167b308fbe1
Andre Macena <andrmacena_@outlook.com>
2023-04-04 22:34:50 -0300

    Finalizado prototipacao da aplicacao e organizado o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignUpPageComponent } from './pages/account/sign-up-page/sign-up-page.component';
+import { PlayersPageComponent } from './pages/fantasy/players-page/players-page.component';
+import { TeamPageComponent } from './pages/fantasy/team-page/team-page.component';
+import { LeaderboardPageComponent } from './pages/fantasy/leaderboard-page/leaderboard-page.component';
+import { HomePageComponent } from './pages/fantasy/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignUpPageComponent,
+    PlayersPageComponent,
+    TeamPageComponent,
+    LeaderboardPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
