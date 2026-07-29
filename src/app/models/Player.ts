914a7a0d74a85125b655f1601bd8d9ce1458d321
André Macena <andremacena97@gmail.com>
2026-07-28 21:01:46 -0300

    feat: add props to player

--- src/app/models/Player.ts
+++ src/app/models/Player.ts
@@ -1,15 +1,32 @@
 export class Player {
+    id: number;
     nickname: string;
-    urlProfile: string;
-    team: string;
+    pathProfile: string;
+    rating: number;
+    acs: number;
+    kills: number;
+    deaths: number;
+    assists: number;
+    kast: number;
+    adr: number;
+    fb: number;
+    fd: number;
     score: number;
-    urlLogoTeam?: string;
 
-    constructor(nickname: string, urlProfile: string, team: string, score: number, urlLogoTeam?: string) {
+
+    constructor(nickname: string, pathProfile: string, score: number, id: number, rating: number, acs: number, kills: number, deaths: number, assists: number, kast: number, adr: number, fb: number, fd: number) {
         this.nickname = nickname;
-        this.team = team;
-        this.urlProfile = urlProfile;
+        this.pathProfile = pathProfile;
         this.score = score;
-        this.urlLogoTeam = urlLogoTeam;
+        this.id = id;
+        this.rating = rating;
+        this.acs = acs;
+        this.kills = kills;
+        this.deaths = deaths;
+        this.assists = assists;
+        this.kast = kast;
+        this.adr = adr;
+        this.fb = fb;
+        this.fd = fd;
     }
 }
